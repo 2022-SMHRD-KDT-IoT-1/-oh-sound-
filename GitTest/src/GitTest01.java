71ff13d0202f90e57186af26b7e4d0a00466f724
SMHRD <SMHRD@DESKTOP-5PNUMOD>
2022-02-18 17:31:32 +0900

    2022/02/18 메인페이지

--- GitTest/src/GitTest01.java
+++ GitTest/src/GitTest01.java
@@ -4,8 +4,11 @@ public class GitTest01 {
 	public static void main(String[] args) {
 		System.out.println("첫번째 커밋!");
 		System.out.println("두번째 커밋!");
-		System.out.println("세번째 커밋!");
 		
+		System.out.println("세번째 커밋!");
+		// 꼭 모든 작업은 커밋 후에 일어난다!!
+		
+		// pull 하기전에 변경사항이 있는지 확인
 		
 		
 	}
